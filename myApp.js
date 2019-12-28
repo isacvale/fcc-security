12911abd73ce701de9f1cec9760ac0b360ee5f7b
isacvale <isac@ocastudios.com>
2019-12-27 22:28:28 -0300

    Fixes port issue

--- myApp.js
+++ myApp.js
@@ -5,13 +5,19 @@ const xDaysInSeconds = days => days * 24 * 60 * 60
 const ninetyDaysInSeconds = xDaysInSeconds(90)
 
 module.exports = app
+
 const api = require('./server.js')
 app.use(express.static('public'))
-app.disable('strict-trnasport-security')
+app.disable('strict-transport-security')
 app.use('/api', api)
+
+
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html')
 })
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('App listenas @ ' + listener.address().port())
+
+const listener = app.listen(
+  process.env.PORT || 3000,
+  function () {
+    console.log('App listenas @ ' + listener.address().port)
 })
