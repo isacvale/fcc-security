d8226730e2b23d1d8ed734edf8a8a92620fb048f
isacvale <isac@ocastudios.com>
2019-12-27 22:33:17 -0300

    requires helmet

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+
+/*
+  1) Install and require helmet
+************************************************/
+const helmet = require('helmet')
+
+
 const xDaysInSeconds = days => days * 24 * 60 * 60
 const ninetyDaysInSeconds = xDaysInSeconds(90)
 
